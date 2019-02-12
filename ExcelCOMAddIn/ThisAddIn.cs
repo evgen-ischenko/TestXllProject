ded3eadb68381a860bd222b0b6b0ad9ca0fc854e
Evgen Ischenko <evgen.ischenko@gmail.com>
2019-02-12 17:55:23 +0200

    initial commit

---
+++ ExcelCOMAddIn/ThisAddIn.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+
+namespace ExcelCOMAddIn
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            var filename = "c:\\workspace\\VisualStudio\\TestXllProject\\TestXllProject\\TestXllProject\\bin\\Debug\\TestXllProject-AddIn.xll";
+            Application.RegisterXLL(filename);
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+            
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+        
+        #endregion
+    }
+}
---
+++ TestXllProject/AddIn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ExcelDna.Integration;
+using ExcelDna.IntelliSense;
+
+namespace TestXllProject
+{
+    public class AddIn : IExcelAddIn
+    {
+
+        public void AutoOpen()
+        {
+            IntelliSenseServer.Install();
+        }
+
+        public void AutoClose()
+        {
+            IntelliSenseServer.Uninstall();
+        }
+
+        [ExcelFunction(description: "Just for test")]
+        public static object TestFunction([ExcelArgument (description: "Some string")] string name)
+        {
+            return "Hello " + name;
+        }
+
+    }
+}
